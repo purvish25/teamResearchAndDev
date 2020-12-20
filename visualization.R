3b4de5a60d575fd3f455607f2694f05584db34ea
Rahul Soni <rs20acs@herts.ac.uk>
2020-12-20 21:27:06 +0000

    Additional Plots for Fianl Report

--- visualization.R
+++ visualization.R
@@ -136,6 +136,54 @@ task3 <- ggplot(df_agegroup_gender, mapping = aes(x = age, y = suicide_per_100k,
   theme(axis.text.x = element_text(angle = 90)) +
   theme(legend.position = "bottom")
 
+################################################################################
+## Task 5 - Suicides rates/100k trends over the years
+################################################################################
+task5 <- suicide_dataset %>%
+  group_by(year) %>%
+  summarize(population = sum(population), 
+            suicides = sum(suicides_no), 
+            suicides_per_100k = (suicides / population) * 100000) %>%
+  
+  ggplot(aes(x = year, y = suicides_per_100k)) + 
+  geom_line(col = "#0072B2", size = 1) + 
+  geom_point(col = "#0072B2", size = 2) + 
+  geom_hline(yintercept = global_average, linetype = 2, color = "#000000", size = 1) +
+  labs(title = "Global Suicides (per 100k)",
+       subtitle = "Trend over time, 1985 - 2015.",
+       x = "Year", 
+       y = "Suicides per 100k") + 
+  scale_x_continuous(breaks = seq(1985, 2015, 2)) 
+
+################################################################################
+## Task 6 - Suicides rates/100k trends for all age groups every 5 years
+################################################################################
+top_20percent_suicides <- suicide_dataset %>%
+  mutate(suicides_per_100k = suicides_no * 100000 / population) %>%
+  arrange(desc(suicides_per_100k)) %>%
+  head(n = round(nrow(.) * 20 / 100))
+
+top_20percent_suicides$time <- ifelse(top_20percent_suicides$year <= 1990, "1985 - 1990", 
+                                      ifelse(top_20percent_suicides$year <= 1995, "1991 - 1995", 
+                                             ifelse(top_20percent_suicides$year <= 2000, "1996 - 2000",
+                                                    ifelse(top_20percent_suicides$year <= 2005, "2001 - 2005",
+                                                           ifelse(top_20percent_suicides$year <= 2010, "2006 - 2010",
+                                                                  "2011 - 2015")))))
+
+View(top_20percent_suicides)
+
+task6 <- ggplot(top_20percent_suicides, aes(x = age, fill = sex)) + 
+  geom_bar() + 
+  labs(title = "20% most significant suicides/100k", 
+       subtitle = "Volumes every 5 years, Age & Sex",
+       x = "Age", 
+       y = "Number of Demographics", 
+       fill = "Sex") + 
+  facet_wrap(~ time) + 
+  scale_y_continuous(breaks = seq(0, 300, 25)) + 
+  theme(legend.position = "bottom") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
 
 ################################################################################################################
 #Task 4 - Collecting all the plots and putting then in a visualization.pdf file
@@ -146,6 +194,8 @@ pdf("visualization.pdf")
 print(task1)
 print(task2)
 print(task3)
+print(task5)
+print(task6)
 
 dev.off()
 
